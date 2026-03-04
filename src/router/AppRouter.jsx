27665f07569b370f7bd0a3798602ac2deaeaf796
Sameer Patariya <sameer.p@newtrendscommerce.in>
2026-03-04 14:52:26 +0530

    Protect entire app with login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,17 +5,27 @@ import HomePage from "../pages/HomePage";
 import ServicesPage from "../pages/ServicesPage";
 import CataloguePage from "../pages/CataloguePage";
 import LoginPage from "../pages/LoginPage";
+import ProtectedRoute from "../components/ProtectedRoute";
 
 function AppRouter() {
   return (
     <Routes>
 
+      {/* Login Page */}
       <Route path="/login" element={<LoginPage />} />
 
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/catalogue" element={<CataloguePage />} />
+      {/* Everything else requires login */}
+      <Route
+        path="/*"
+        element={
+          <ProtectedRoute>
+            <MainLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<HomePage />} />
+        <Route path="services" element={<ServicesPage />} />
+        <Route path="catalogue" element={<CataloguePage />} />
       </Route>
 
     </Routes>
