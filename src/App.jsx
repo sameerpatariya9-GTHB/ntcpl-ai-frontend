b92046c75aa038420c389287a0a5d5db7a88962e
Sameer Patariya <sameer.p@newtrendscommerce.in>
2026-03-04 15:07:19 +0530

    Force login gate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <AppRouter />;
+
+  const token = localStorage.getItem("token");
+
+  // If no token → show login page only
+  if (!token) {
+    return (
+      <BrowserRouter>
+        <LoginPage />
+      </BrowserRouter>
+    );
+  }
+
+  // If token exists → load full app
+  return (
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,50 +4,15 @@ import MainLayout from "../layout/MainLayout";
 import HomePage from "../pages/HomePage";
 import ServicesPage from "../pages/ServicesPage";
 import CataloguePage from "../pages/CataloguePage";
-import LoginPage from "../pages/LoginPage";
-import ProtectedRoute from "../components/ProtectedRoute";
 
 function AppRouter() {
   return (
     <Routes>
-
-      {/* Public login */}
-      <Route path="/login" element={<LoginPage />} />
-
-      {/* Protected routes */}
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <MainLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<HomePage />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/catalogue" element={<CataloguePage />} />
       </Route>
-
-      <Route
-        path="/services"
-        element={
-          <ProtectedRoute>
-            <MainLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<ServicesPage />} />
-      </Route>
-
-      <Route
-        path="/catalogue"
-        element={
-          <ProtectedRoute>
-            <MainLayout />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<CataloguePage />} />
-      </Route>
-
     </Routes>
   );
 }
